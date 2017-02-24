a2247835abb19cd33d150f461fbb4ce1d9ae921e
wchb2017 <wchb20155@gmail.com>
2017-02-24 20:44:25 +0800

    update

--- src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
+++ src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
@@ -8,13 +8,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by wchb7 on 16-5-9.
- */
-
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring/spring-dao.xml")
 public class SuccessKilledDaoTest {
--- src/test/java/org/seckill/service/SeckillServiceTest.java
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -13,12 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by wchb7 on 16-5-14.
- */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
 public class SeckillServiceTest {
